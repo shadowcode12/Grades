9350929f683619f84734c5df3cd8e2030e0071bc
shadowcode12 <omkar2gi@gmail.com>
2025-01-17 23:11:53 +0530

    Initial commit

---
+++ grades_calculator.py
@@ -0,0 +1,24 @@
+name = input("Enter your name: ")
+marks = int(input("Enter your marks: "))
+grade = ""
+
+if marks >= 90:
+    grade = "A+"
+
+elif marks >= 80:
+    grade = "A"
+
+elif marks >= 70:
+    grade = "B"
+
+elif marks >= 60:
+    grade = "C"
+
+elif marks >= 40:
+    garde = "D"
+
+else:
+    grade = "F"
+    print("You have Failed, Better luck next time")
+
+print(f"Hello! My name is {name}, and my marks is {marks} and my grade is {grade}")
